2545cf09e5f9abbef21d1980f19d535eed2719b1
Brant Marshall <brant.marshall1@gmail.com>
2023-03-30 03:43:05 -0400

    Base  Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Nitesky.xyz
+A landing page for Nitesky.xyz, a personal file storage site and image uploader. Nothing special.
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Nitesky.xyz &raquo; A personal image sharing and file storing database.</title>
+<meta name="author" content="Brant / Nitesky Music">
+<link href="assets/css/global.css" rel="stylesheet" media="all"/>
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" media="screen" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+    <div class="logo">
+        <img src="assets/img/logo.png">
+    </div>
+    <div class="bgbar">
+        <p class="home">HOME</p> <p class="instagram">INSTAGRAM</p> <p class="youtube">YOUTUBE</p> <p class="discord">DISCORD</p> <p class="music">MUSIC</p> <p class="contact">CONTACT</p> <p class="images">IMAGES</p>
+    </div>
+
+</body>
+</html>
